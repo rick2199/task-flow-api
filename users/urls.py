4cff5d6a56aff26cc7dec14aa630750e29919cd8
Ricardo Rojas <richardrojasa@gmail.com>
2023-09-03 15:11:54 -0500

    feat(user): :sparkles: add register and logout paths

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from knox import views as knox_views
 from . import views
 
 urlpatterns = [
     path("login/", views.login),
+    path("user/", views.get_user_data),
+    path("register/", views.register),
+    path("logout/", knox_views.LogoutView.as_view(), name="knox_logout"),
 ]
