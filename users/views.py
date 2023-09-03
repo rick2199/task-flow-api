5edd15e330a4582448376fad2ddcf5b85ca01b80
Ricardo Rojas <richardrojasa@gmail.com>
2023-09-03 14:17:47 -0500

    feat: :tada: init project and set users and login api

---
+++ users/views.py
@@ -0,0 +1,23 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.auth import AuthToken
+
+
+@api_view(["POST"])
+def login(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data["user"]
+    _, token = AuthToken.objects.create(user)
+
+    return Response(
+        {
+            "user_info": {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+            },
+            "token": token,
+        }
+    )
